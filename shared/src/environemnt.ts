da2c28d6bd4beb73bf87ef2a671680441e36479b
Sergei Solovev <sergey.soloviev@inbox.ru>
2022-06-29 22:38:51 +0300

    updated environemnt file

--- shared/src/environemnt.ts
+++ shared/src/environemnt.ts
@@ -2,6 +2,6 @@ export const environment = {
     PORT: 3000,
     CACHE_DURATION: 30,
     BMSTU_ORIGIN: 'https://lks.bmstu.ru',
-    mongoConnectionString: 'mongodb://localhost:27018',
+    mongoConnectionString: 'mongodb://mongo:27017', // TODO: Take value from env
     collectorIntervalMs: 1000 * 60 * 60 * 8 // 1 time per 8 hours  
 }
