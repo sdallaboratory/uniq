de47c16b9623f7489fcc29f62c26a514146f9239
Sergei Solovev <sergey.soloviev@inbox.ru>
2022-09-06 21:40:22 +0300

    client Angular app scaffolded

--- shared/src/environemnt/environment.dev.ts
+++ shared/src/environemnt/environment.dev.ts
@@ -1,4 +1,4 @@
 export const devEnvironemtn = {
     mongoConnectionString: `mongodb://193.187.96.23:27019`, // TODO: Take value from env
-    skipCollectOnStart: false,
+    skipCollectOnStart: true,
 }
